72e2993d96c2a4f15d6d78a2ae09df1616de06e4
loundness <ludo.vic@me.com>
2018-08-02 19:43:41 +0200

    mise en place des emails et mdp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/index', to:'static_pages#index'
+  get '/home', to:'static_pages#home'
+
+
+	#resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
